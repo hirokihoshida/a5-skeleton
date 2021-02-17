6db4e8dd718de788798a4c8ca7dceea5988d8210
Hiroki Hoshida <hiroki.lincoln@gmail.com>
2021-02-17 13:33:39 -0600

    all pages rough draft

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,7 @@ $(document).ready(function(){
     $('.modal').modal();
     $('.datepicker').datepicker();
     $('.timepicker').timepicker();
+    $('#start-stop-btn').click(function(){
+      $('#start-stop-btn').text($('#start-stop-btn').text() == "Start" ? "Stop" : "Start");
+    });
   });
